54aaa5afa58b8991273da27bf04a1a85ee80ca9f
Barun Debnath <barundebnath91@gmail.com>
2025-09-30 23:17:21 +0530

    imported all pages in root App file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,33 @@
 import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import { AuthProvider } from "./contexts/AuthContext";
+import { WorkflowProvider } from "./contexts/WorkflowContext";
+import Credentials from "./pages/Credentials";
+import Dashboard from "./pages/Dashboard";
+import Executions from "./pages/Executions";
+import Login from "./pages/Login";
+import WorkflowEditor from "./pages/WorkflowEditor";
 
 function App() {
 	return (
-		<Router>
-			<div className="min-h-screen bg-gray-50">
-				<Layout />
-				{/* <Routes>
-					<Route path="/" element={<Layout />}>
-                <Route index element={<Navigate to="/dashboard" replace />} />
-								<Route path="dashboard" element={<Dashboard />} />
-              </Route>
-				</Routes> */}
-			</div>
-		</Router>
+		<AuthProvider>
+			<WorkflowProvider>
+				<Router>
+					<div className="min-h-screen bg-gray-50">
+						<Routes>
+							<Route path="/login" element={<Login />} />
+							<Route path="/" element={<Layout />}>
+								<Route index element={<Navigate to="/dashboard" replace />} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="workflow/:id?" element={<WorkflowEditor />} />
+                <Route path="credentials" element={<Credentials />} />
+                <Route path="executions" element={<Executions />} />
+							</Route>
+						</Routes>
+					</div>
+				</Router>
+			</WorkflowProvider>
+		</AuthProvider>
 	)
 }
 
